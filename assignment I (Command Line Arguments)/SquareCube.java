b5519f9eefb33e7d8ea5eb7216b26d8f19000a80
VatsalMandloi <68588254+VatsalMandloi@users.noreply.github.com>
2022-04-29 22:34:31 +0530

    Add files via upload

---
+++ assignment I (Command Line Arguments)/SquareCube.java
@@ -0,0 +1,15 @@
+//assignmet 1.2 
+// wap to enter n value from command line and find square and cube of given number and 
+//display results in descending order.
+public class SquareCube {
+    public static void main(String[] args) {
+        if (args.length > 0) {
+            int len = (args.length);
+            int a;
+            for (int i = len - 1; i >= 0; i--) {
+                a = Integer.parseInt(args[i]);
+                System.out.println(" Square:" + (a * a) + ", Cube:" + (a * a * a));
+            }
+        }
+    }
+}
